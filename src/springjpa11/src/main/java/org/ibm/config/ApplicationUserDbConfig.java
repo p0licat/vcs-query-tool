4def8d0f41007aa0d75d3824b384fcb45a271132
Septimiu-Calin Bodica <p0licat@users.noreply.github.com>
2022-07-11 21:10:33 +0300

    Configuration class with various bean. Package searching. @Configuration tag. Does Spring have global @Configuration search? No @Configuration search in @SpringApplication.

--- src/springjpa11/src/main/java/org/ibm/config/ApplicationUserDbConfig.java
+++ src/springjpa11/src/main/java/org/ibm/config/ApplicationUserDbConfig.java
@@ -1,12 +1,22 @@
 package org.ibm.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
@@ -19,4 +29,19 @@ public class ApplicationUserDbConfig {
 	public DataSource dataSource() {
 		return DataSourceBuilder.create().build();
 	}
+
+	@Primary
+	@Bean(name="entityManagerFactory")
+	public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(EntityManagerFactoryBuilder builder, @Qualifier("datasource") DataSource dataSource) {
+		Map<String, Object> properties = new HashMap<>();
+		properties.put("hibernate.hbm2ddl.auto", "update");
+		properties.put("hibernate.dialect", "org.hibernate.dialect.SQLServer2012Dialect");
+		return builder.dataSource(dataSource).properties(properties).packages("org.ibm.model.applicationuser").persistenceUnit("ApplicationUser").build();
+	}
+
+	@Bean(name="transactionManager")
+	public PlatformTransactionManager transactionManager(@Qualifier("entityManagerFactory") EntityManagerFactory entityManagerFactory) {
+		return new JpaTransactionManager(entityManagerFactory);
+	}
+	
 }
