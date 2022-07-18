e23f6bdf97fd394a5da0c3d336749b78f969bfa0
Septimiu-Calin Bodica <p0licat@users.noreply.github.com>
2022-07-18 19:46:39 +0300

    Specify business logic for complete scanning of repository.

--- src/springjpa11/src/main/java/org/ibm/SpringjpaApplication.java
+++ src/springjpa11/src/main/java/org/ibm/SpringjpaApplication.java
@@ -6,7 +6,9 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.ibm.model.deserializers.contentservice.GetRepoContentsDeserializerFromGithubReply;
@@ -34,10 +36,10 @@ import com.fasterxml.jackson.databind.module.SimpleModule;
 @ComponentScan(basePackages = "{org.ibm.*}")
 @EntityScan("org.ibm.*")
 public class SpringjpaApplication {
-	
+
 	@Autowired
 	private GitRepoRepository gitRepoRepository;
-	
+
 	private ObjectMapper getMapperFor__getRepoContentsDeserializer() {
 		ObjectMapper mapper = new ObjectMapper();
 		SimpleModule module = new SimpleModule();
@@ -53,31 +55,68 @@ public class SpringjpaApplication {
 
 		return httpClient.send(request, BodyHandlers.ofString());
 	}
-	
+
 	@PostMapping("/populateUserRepositories")
-	public RequestUserRepositoriesDTO requestUserRepositoryData(String username, String repoName) throws IOException, InterruptedException {
-		
-		List<GitRepository> repos = gitRepoRepository.findAll().stream().filter(e -> e.getName().contains(repoName)).collect(Collectors.toList());
+	public RequestUserRepositoriesDTO requestUserRepositoryData(String username, String repoName)
+			throws IOException, InterruptedException {
+
+		List<GitRepository> repos = gitRepoRepository.findAll().stream().filter(e -> e.getName().contains(repoName))
+				.collect(Collectors.toList());
 		String foundName = repos.get(0).getName();
-		
-		String response = this.makeRequest("http://127.0.0.1:8081/getContentsOfRepo?username=" + username + "&repoName=" + repoName.toString()).body();
-		
+
+		String response = this.makeRequest(
+				"http://127.0.0.1:8081/getContentsOfRepo?username=" + username + "&repoName=" + repoName.toString())
+				.body();
+
+		Set<String> performedRequests = new HashSet<>(); // maybe this container should have versioned persistence or make this method responsible for completeness
+		// for example creating a stack of get...ofDirectory and looping while there are changes to the stack. note that the loop should not mutate the stack, but return a new one
+
 		ObjectMapper mapper = this.getMapperFor__getRepoContentsDeserializer();
 		var lombokDeserialized = mapper.readValue(response, RepoContentsFromGithubReplyDTO.class);
 		lombokDeserialized.getNodes().forEach(e -> {
 			if (e.getType().equals("dir")) {
 				// either use recursion or a queue of requests
-				// must also create a new route in the contentscanner /getContentsOfRepoAtContentsUrl?...
+				// must also create a new route in the contentscanner
+				// /getContentsOfRepoAtContentsUrl?...
+
+				// move business logic elsewhere...
+				if (!performedRequests.contains(e.getContentsUrl())) {
+
+					try {
+						String result = this
+								.makeRequest("http://127.0.0.1:8081/getContentsOfRepoAtContentsUrlOfFile?username="
+										+ username + "&contentsUrl=" + e.getContentsUrl())
+								.body();
+						// result.persist()
+						performedRequests.add(e.getContentsUrl());
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					} catch (InterruptedException e1) {
+						e1.printStackTrace();
+					}
+				} else {
+					try {
+						String result = this
+								.makeRequest("http://127.0.0.1:8081/getContentsOfRepoAtContentsUrlOfDirectory?username="
+										+ username + "&contentsUrl=" + e.getContentsUrl())
+								.body();
+						performedRequests.add(e.getContentsUrl());
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					} catch (InterruptedException e1) {
+						e1.printStackTrace();
+					}
+				}
 			}
 		});
-		
+
 		return null;
 	}
-	
+
 	@PostMapping("/populateUserDetails")
 	public RequestUserDetailsDTO requestUserDetailsData(String username) {
 		return null;
-		
+
 	}
 
 	public static void main(String[] args) {
