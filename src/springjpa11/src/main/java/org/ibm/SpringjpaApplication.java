07f64a5e5bae8d2c26a9e943c7103f319d5defce
Septimiu-Calin Bodica <p0licat@users.noreply.github.com>
2022-07-19 11:08:56 +0300

    Introspecting java code with debugger to understand performance of business logic. Missing endpoint route. (partial execution)

--- src/springjpa11/src/main/java/org/ibm/SpringjpaApplication.java
+++ src/springjpa11/src/main/java/org/ibm/SpringjpaApplication.java
@@ -9,9 +9,12 @@ import java.net.http.HttpResponse.BodyHandlers;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.Stack;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 import org.ibm.model.deserializers.contentservice.GetRepoContentsDeserializerFromGithubReply;
+import org.ibm.model.deserializers.contentservice.model.RepoContentsFromEndpointResponseDTO;
 import org.ibm.model.deserializers.contentservice.model.RepoContentsFromGithubReplyDTO;
 import org.ibm.model.repohub.GitRepository;
 import org.ibm.repository.GitRepoRepository;
@@ -37,6 +40,8 @@ import com.fasterxml.jackson.databind.module.SimpleModule;
 @EntityScan("org.ibm.*")
 public class SpringjpaApplication {
 
+	Logger logger = Logger.getLogger(getClass().getName());
+	
 	@Autowired
 	private GitRepoRepository gitRepoRepository;
 
@@ -61,7 +66,7 @@ public class SpringjpaApplication {
 			throws IOException, InterruptedException {
 
 		List<GitRepository> repos = gitRepoRepository.findAll().stream().filter(e -> e.getName().contains(repoName))
-				.collect(Collectors.toList());
+				.collect(Collectors.toList()); // todo optimize query by creating a custom query within repo
 		String foundName = repos.get(0).getName();
 
 		String response = this.makeRequest(
@@ -70,11 +75,14 @@ public class SpringjpaApplication {
 
 		Set<String> performedRequests = new HashSet<>(); // maybe this container should have versioned persistence or make this method responsible for completeness
 		// for example creating a stack of get...ofDirectory and looping while there are changes to the stack. note that the loop should not mutate the stack, but return a new one
-
+		Stack<String> queryQueue = new Stack<>();
+		
 		ObjectMapper mapper = this.getMapperFor__getRepoContentsDeserializer();
-		var lombokDeserialized = mapper.readValue(response, RepoContentsFromGithubReplyDTO.class);
+		var lombokDeserialized = mapper.readValue(response, RepoContentsFromEndpointResponseDTO.class);
+		
+		// todo: add while loop
 		lombokDeserialized.getNodes().forEach(e -> {
-			if (e.getType().equals("dir")) {
+			if (e.getType().equals("file")) {
 				// either use recursion or a queue of requests
 				// must also create a new route in the contentscanner
 				// /getContentsOfRepoAtContentsUrl?...
@@ -89,23 +97,37 @@ public class SpringjpaApplication {
 								.body();
 						// result.persist()
 						performedRequests.add(e.getContentsUrl());
+						logger.info("Found a file: " + e.getContentsUrl());
+						logger.info("Found a file: " + e.getDownloadsUrl());
 					} catch (IOException e1) {
 						e1.printStackTrace();
 					} catch (InterruptedException e1) {
 						e1.printStackTrace();
 					}
-				} else {
-					try {
-						String result = this
-								.makeRequest("http://127.0.0.1:8081/getContentsOfRepoAtContentsUrlOfDirectory?username="
-										+ username + "&contentsUrl=" + e.getContentsUrl())
-								.body();
-						performedRequests.add(e.getContentsUrl());
-					} catch (IOException e1) {
-						e1.printStackTrace();
-					} catch (InterruptedException e1) {
-						e1.printStackTrace();
-					}
+				}
+				
+				
+			} else {
+				// directory request branch
+				try {
+					String result = this
+							.makeRequest("http://127.0.0.1:8081/getContentsOfRepoAtContentsUrlOfDirectory?username="
+									+ username + "&contentsUrl=" + e.getContentsUrl())
+							.body();
+					performedRequests.add(e.getContentsUrl());
+					var currentRequestDeserialized = mapper.readValue(result, RepoContentsFromEndpointResponseDTO.class);
+					currentRequestDeserialized.getNodes().forEach(node -> {
+						logger.info("Sub-file of directory with name: " + e.getName() + " is: " + node.getContentsUrl());
+						if (!performedRequests.contains(node.getContentsUrl())) {
+							queryQueue.add(node.getContentsUrl());
+							logger.info("Directory: added to query queue the url: " + node.getContentsUrl());
+						}
+					});
+					
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				} catch (InterruptedException e1) {
+					e1.printStackTrace();
 				}
 			}
 		});
