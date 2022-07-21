2821a71bd912b7a348ce4ac7d4cc6579d0c7614d
Septimiu-Calin Bodica <p0licat@users.noreply.github.com>
2022-07-21 14:20:01 +0300

    Master RestController with shared resources. Start refactoring. Experiment with shared resources.

---
+++ src/vcs_ui/src/main/java/org/ibm/vcs_ui/VcsUiApplication.java
@@ -0,0 +1,13 @@
+package org.ibm.vcs_ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VcsUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VcsUiApplication.class, args);
+	}
+
+}
