a48cf5fc6d99a5bea31050cc583eb444e2c13ea0
Septimiu-Calin Bodica <p0licat@users.noreply.github.com>
2022-08-04 12:08:39 +0300

    Frontend project structure.

---
+++ src/frontend/src/features/pages/SearchRepos.tsx
@@ -0,0 +1,9 @@
+export interface SearchReposPageProps {}
+
+export function SearchRepos(props: SearchReposPageProps) {
+    return (
+        <div>
+            asdf
+        </div>
+    );
+}
---
+++ src/frontend/src/features/pages/Users.tsx
@@ -0,0 +1,9 @@
+export interface UsersPageProps {}
+
+export function Users(props: UsersPageProps) {
+    return (
+        <div>
+            asdf
+        </div>
+    );
+}
