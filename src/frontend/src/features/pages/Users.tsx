2f6570200c303c9609bd2c41c3d65d9ca6771144
Septimiu-Calin Bodica <p0licat@users.noreply.github.com>
2022-08-04 15:31:18 +0300

    Test web requests. Add CORS to backend.

--- src/frontend/src/features/pages/Users.tsx
+++ src/frontend/src/features/pages/Users.tsx
@@ -1,15 +1,24 @@
 import { Button } from "@mui/material";
+import { store } from "../../app/store";
 import { AddUserToRepoElement } from "../components/operations/AddUserToRepoElement";
 import { ListUsersElement } from "../components/searchable/ListUsersElement";
+import { fetchUsers } from "../slices/usersSlice/usersSlice";
 
 export interface UsersPageProps {}
 
 export function Users(props: UsersPageProps) {
+  const loadUsers = () => {
+    console.log("load");
+    store.dispatch<any>(fetchUsers());
+  };
+
   return (
     <div>
       <div id="components">
         <AddUserToRepoElement />
-        <Button variant="contained">Load users</Button>
+        <Button variant="contained" onClick={loadUsers}>
+          Load users
+        </Button>
         <ListUsersElement />
       </div>
       Users page.
--- src/frontend/src/features/slices/usersSlice/usersSlice.tsx
+++ src/frontend/src/features/slices/usersSlice/usersSlice.tsx
@@ -1,6 +1,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 import { RootState } from "../../../app/store";
+import { GetUsersDTO } from "../../components/model/dto/GetUsersDTO";
 import { UserDTO } from "../../components/model/dto/UserDTO";
 
 export interface UsersSliceState {
@@ -12,10 +13,13 @@ const initialState: UsersSliceState = {
 };
 
 export const fetchUsers = createAsyncThunk("eventList/fetchUsers", async () => {
-  const response = await axios.get<UserDTO>(
-    `https://${process.env.REACT_APP_ADDRESS_OF_USER_DETAILS_ENDPOINT}:${process.env.REACT_APP_ADDRESS_OF_USER_DETAILS_ENDPOINT_PORT}/getUsers`
+  const response = await axios.get<GetUsersDTO>(
+    `http://${process.env.REACT_APP_ADDRESS_OF_USER_DETAILS_ENDPOINT}:${process.env.REACT_APP_ADDRESS_OF_USER_DETAILS_ENDPOINT_PORT}/getUsers`
   );
-
+  console.log(
+    `http://${process.env.REACT_APP_ADDRESS_OF_USER_DETAILS_ENDPOINT}:${process.env.REACT_APP_ADDRESS_OF_USER_DETAILS_ENDPOINT_PORT}/getUsers`
+  );
+  console.log(response);
   return response.data;
 });
 
@@ -27,7 +31,7 @@ export const usersSlice = createSlice({
     builder
       .addCase(fetchUsers.pending, (state) => {})
       .addCase(fetchUsers.fulfilled, (state, action) => {
-        state.usersList = [action.payload];
+        state.usersList = action.payload.userList;
       });
   },
 });
