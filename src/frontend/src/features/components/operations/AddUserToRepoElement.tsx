439149dc940e576f09513566ae63d4feca3bd36e
Septimiu-Calin Bodica <p0licat@users.noreply.github.com>
2022-08-04 16:19:43 +0300

    Search element for gathering a user's public repos.

--- src/frontend/src/features/components/operations/AddUserToRepoElement.tsx
+++ src/frontend/src/features/components/operations/AddUserToRepoElement.tsx
@@ -1,5 +1,12 @@
+import { Button, TextField } from "@mui/material";
+
 export interface AddUserToRepoElementProps {}
 
 export function AddUserToRepoElement(props: AddUserToRepoElementProps) {
-  return <div>Add users element.</div>;
+  return (
+    <div>
+      <TextField />
+      <Button variant="contained">Gather</Button>
+    </div>
+  );
 }
