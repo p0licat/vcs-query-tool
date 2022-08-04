d7020e6c84b2b69f51e31ea6091dac39f428c0b1
Septimiu-Calin Bodica <p0licat@users.noreply.github.com>
2022-08-04 14:17:09 +0300

    Combined reducers with usersSlice.

--- src/frontend/src/app/store.ts
+++ src/frontend/src/app/store.ts
@@ -1,11 +1,16 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/slices/counter/counterSlice';
+import { Action, applyMiddleware, combineReducers, createStore, ThunkAction } from '@reduxjs/toolkit';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from "redux-thunk";
+import usersSlice from '../features/slices/usersSlice/usersSlice';
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+export const combinedstores = combineReducers({
+  usersSliceReducer: usersSlice
 });
+
+export const store = createStore(
+  combinedstores,
+  composeWithDevTools(applyMiddleware(thunk)) // not needed in production
+)
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
---
+++ src/frontend/src/features/slices/usersSlice/usersSlice.tsx
@@ -0,0 +1,38 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { RootState } from "../../../app/store";
+import { UserDTO } from "../../components/model/dto/UserDTO";
+
+export interface UsersSliceState {
+  usersList: Array<UserDTO>;
+}
+
+const initialState: UsersSliceState = {
+  usersList: [],
+};
+
+export const fetchUsers = createAsyncThunk("eventList/fetchUsers", async () => {
+  const response = await axios.get<UserDTO>(
+    `https://${process.env.REACT_APP_ADDRESS_OF_USER_DETAILS_ENDPOINT}:${process.env.REACT_APP_ADDRESS_OF_USER_DETAILS_ENDPOINT_PORT}/getUserDetails?username=` +
+      "p0licat"
+  );
+
+  return response.data;
+});
+
+export const usersSlice = createSlice({
+  name: "users",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchUsers.pending, (state) => {})
+      .addCase(fetchUsers.fulfilled, (state, action) => {
+        state.usersList = [action.payload];
+      });
+  },
+});
+
+export const usersList = (state: RootState) =>
+  state.usersSliceReducer.usersList;
+export default usersSlice.reducer;
