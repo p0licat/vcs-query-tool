b38041a6c1273801f82a24291affcb804c242d57
Septimiu-Calin Bodica <p0licat@users.noreply.github.com>
2022-08-12 15:52:59 +0300

    Update init_script.sh

--- init_script.sh
+++ init_script.sh
@@ -8,9 +8,9 @@ echo "BACKEND_DB_CONN_ADDR=127.0.0.1" > .env
 echo "BACKEND_DB_CONN_PASS=$pwd1" >> .env
 
 echo "spring.datasource.password=$pwd1" > ./src/springjpa11/src/main/resources/application_password.properties
-echo "mesh.NETWORK_ADDR=172.18.0.1" >> ./src/springjpa11/src/main/resources/application_connection_urls.properties
 
 sed s/localhost/172.18.0.1/g ./src/springjpa11/src/main/resources/application.properties | tee ./src/springjpa11/src/main/resources/application.properties
+sed s/mesh.NETWORK_ADDR=127.0.0.1/mesh.NETWORK_ADDR=172.18.0.1/g ./src/springjpa11/src/main/resources/application_connection_urls.properties | tee ./src/springjpa11/src/main/resources/application_connection_urls.properties
 
 echo "Done."
 cat .env
