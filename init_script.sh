c92401a0d682cf5d44b12480834854e30f6ba7cb
Septimiu-Calin Bodica <p0licat@users.noreply.github.com>
2022-08-13 16:39:05 +0300

    Update init_script.sh to perform the necessary insertions of security access tokens in their corresponding Java applications.

--- init_script.sh
+++ init_script.sh
@@ -3,6 +3,8 @@ touch .env
 
 echo "Insert password: "
 read pwd1
+echo "Insert secret store key: "
+read sec1 # for this arg, visit secret store or in my case the private repo / drive url
 
 echo "BACKEND_DB_CONN_ADDR=127.0.0.1" > .env
 echo "BACKEND_DB_CONN_PASS=$pwd1" >> .env
@@ -12,6 +14,13 @@ echo "spring.datasource.password=$pwd1" > ./src/springjpa11/src/main/resources/a
 sed s/localhost/172.18.0.1/g ./src/springjpa11/src/main/resources/application.properties | tee ./src/springjpa11/src/main/resources/application.properties
 sed s/mesh.NETWORK_ADDR=127.0.0.1/mesh.NETWORK_ADDR=172.18.0.1/g ./src/springjpa11/src/main/resources/application_connection_urls.properties | tee ./src/springjpa11/src/main/resources/application_connection_urls.properties
 sed s/REACT_APP_ADDRESS=127.0.0.1/REACT_APP_ADDRESS=172.18.0.1/g ./src/frontend/.env | tee ./src/frontend/.env
+
+gpg -o appKey.txt.retrieve --passphrase "$sec1" --batch --yes -d appKey.txt.gpg
+cp appKey.txt.retrieve ./src/contentscanner/src/main/resources/appKey.txt
+
+# to restrict necessary permissions, no cleanup is performed.
+# rm ./*.retrieve 
+# rm ./*.gpg
 
 
 echo "Done."
